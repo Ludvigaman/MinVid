517d13e7f61956389cb0a2dc7e95f6fab229be2a
Ludvig Åman <lula@netcompany.com>
2025-05-14 09:48:03 +0200

    Started on the image service

---
+++ MinVid-API/Services/ImageService.cs
@@ -0,0 +1,13 @@
+﻿namespace MinVid_API.Services
+{
+    public class ImageService
+    {
+        private readonly string _dataPath;
+
+        public ImageService(IConfiguration configuration)
+        {
+            _dataPath = configuration.GetValue<string>("image_data_path");
+        }
+
+    }
+}
